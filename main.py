97a27ba0206b773369c598b2082353f26e7606aa
Cubinec-py <Ujyxfhtyrj95>
2022-06-15 16:37:58 +0300

    tas_2

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask
+from flask import Flask, request
+from faker import Faker
 
 import csv
 
 app = Flask(__name__)
+fake_elements = Faker()
 
 
 @app.route("/requirements/")
@@ -13,6 +15,15 @@ def requirements():
         for i in a:
             value.append(', '.join(i))
     return '<p>\n<p>'.join(value)
+
+
+@app.route("/generate-users/")
+def generator():
+    arg_from_get = request.args.get('count', '')
+    email_lst = []
+    for i in range(int(arg_from_get)):
+        email_lst.append(f'{fake_elements.ascii_free_email()}')
+    return '<p>\n<p>'.join(email_lst)
 
 
 if __name__ == '__main__':
