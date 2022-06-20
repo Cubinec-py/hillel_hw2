4793ea57b86cc44005fbe95a12e2ec56599d747b
Cubinec-py <Ujyxfhtyrj95>
2022-06-20 15:24:54 +0300

    task_1

---
+++ flaskr/data_in.sql
@@ -0,0 +1,12 @@
+INSERT INTO tracks (title, artist, genres, length)
+VALUES
+  ('Плачу на техно', 'Cream Soda & ХЛЕБ', 'pop', 160),
+  ('Розовый фламинго', 'Cream Soda & Алёна Свиридова', 'pop', 224),
+  ('Никаких больше вечеринок', 'Cream Soda', 'dance', 227),
+  ('Purple Noise', 'Boris Brejcha', 'dance', 540),
+  ('Bruci come incendio', 'SLAVA MARLOW', 'pop', 169),
+  ('I’m OK', 'Little Big', 'pop', 187),
+  ('Go Bananas', 'Little Big', 'pop', 162),
+  ('Ренессанс', 'Иван Дорн & Роман Бестселлер', 'pop', 227),
+  ('Стыцамен', 'Иван Дорн', 'pop', 233),
+  ('Back In Black', 'AC/DC', 'hard_fluskrock', 256);
